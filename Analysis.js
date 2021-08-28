efab6306b3153fa90ca3f0c02392cc716f17ffad
mathe <mateo.colmenares@udea.edu.co>
2021-08-27 22:27:10 -0500

    Finishing out Salary analysis 27.08-2021

--- Analysis.js
+++ Analysis.js
@@ -24,7 +24,32 @@ function MedianSalary(lista){
     if (esPar(lista.length)== true){
         const personaMitad1 = lista[mitad-1];
         const personaMitad2= lista[mitad];
+        return (personaMitad1 + personaMitad2)/2;
     } else {
         const personaMitad = lista[mitad];
+        return personaMitad;
     }
-};
+};  
+
+//We need to create a variable for our general median.
+const MedianaGeneralColombia = MedianSalary(SalariesColombiaSorted)
+
+//To calculate our top 10%, we need our sorted array and certain number of positions.
+//To use the method Splice we need to define where are we going to start
+const SpliceStart = SalariesColombiaSorted.length * (90/100); //To our total length we calculate the 90%
+//Now we need to define the number of the positions
+const SpliceCount = SalariesColombiaSorted.length - SpliceStart; //The cut is the difference
+
+const SalariosColTop10 = SalariesColombiaSorted.splice(
+    SpliceStart,
+    SpliceCount
+);
+
+//Calculate the top 10% median
+const MedianaTop10Colombia = MedianSalary(SalariosColTop10);
+
+//At the end we're going to print our general median and our top 10%.
+console.log({
+    MedianaGeneralColombia,
+    MedianaTop10Colombia,
+});
